e4f9bdcef5acafc13c7ad479ffbc60d0ffc8832c
Daniel <daniel.roa98@gmail.com>
2021-11-08 10:45:46 -0600

    Updated files

--- src/firebase/configFirebase.js
+++ src/firebase/configFirebase.js
@@ -3,30 +3,27 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-/* const firebaseConfig = {
-    apiKey: "AIzaSyDVUeYVFvwUWyzH1adKIc5D-Ae7qP8M30k",
-    authDomain: "poc-farmacias-sanate.firebaseapp.com",
-    projectId: "poc-farmacias-sanate",
-    storageBucket: "poc-farmacias-sanate.appspot.com",
-    messagingSenderId: "340180046991",
-    appId: "1:340180046991:web:ec8dcd91343db49d554cfa",
-    measurementId: "G-PJH0G77D1K"
-}; */
+export const api_key = process.env.REACT_APP_APIKEY;
+export const auth_domain = process.env.REACT_APP_AUTHDOMAIN;
+export const projectId = process.env.REACT_APP_PROJECTID;
+export const storageBucket = process.env.REACT_APP_STORAGEBUCKET;
+export const messengerId = process.env.REACT_APP_MESSAGINGSENDERID;
+export const appId = process.env.REACT_APP_ID;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4YIG1yEYcKe0jNE7ogsVAuAZMJWhc1A0",
-  authDomain: "proyectofinal-labweb.firebaseapp.com",
-  projectId: "proyectofinal-labweb",
-  storageBucket: "proyectofinal-labweb.appspot.com",
-  messagingSenderId: "732757661882",
-  appId: "1:732757661882:web:204e6d548d79d3da22ca83"
+    apiKey: api_key,
+    authDomain: auth_domain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messengerId,
+    appId: appId
 };
 
 let instance;
 
 export default function getFirebase() {
-    if(typeof window !== 'undefined'){
-        if(instance) return instance;
+    if (typeof window !== 'undefined') {
+        if (instance) return instance;
         instance = firebase.initializeApp(firebaseConfig);
         return instance;
     }
